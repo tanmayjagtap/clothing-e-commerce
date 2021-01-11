c8ef781955a56e887ca55cdc103a3ce87d432273
tanmayjagtap <tanmayjagtap27@gmail.com>
2021-01-11 12:43:45 +0530

    Styling HomePgae

---
+++ src/Components/Menu-item/Menu-item.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Meni-item.scss'
+
+const MenuItem = ({title,imageUrl,size}) =>(
+    <div className={`${size} menu-item`}>
+    <div
+    className='background-image'
+    style={{backgroundImage:`url(${imageUrl})`}}
+    />
+    <div className='content'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+        <span className='subtitle'>Show Now</span>
+    </div>
+</div>
+)
+
+export default MenuItem;
