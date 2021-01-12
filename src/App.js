f6ffef4dd2e775daa7054a92a9d7eb4f89f8ad06
tanmayjagtap <tanmayjagtap27@gmail.com>
2021-01-12 12:12:57 +0530

    Added Route to Hats Basic Version and Font updated to lato

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 import HomePage from './Pages/HomePage/HomePage';
+import {Route,Switch} from 'react-router-dom';
+
+const Hats = () =>{
+  return(
+    <div>
+      Hats Page
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch/>
+      <Route exact path='/' component={HomePage}/>
+      <Route exact path='/hats' component={Hats}/>
+      <Switch/>
     </div>
   );
 }
